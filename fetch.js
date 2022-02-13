1554e6603e8adb736a76c509dd3aed02bf7f41bd
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-02-13 17:24:19 -0300

    requisição do resultado da tabela fipe concluida

--- fetch.js
+++ fetch.js
@@ -15,9 +15,25 @@ const modelVeicle = async (codigoVeicle, veicle) => {
     const response = await fetch(url);
     const data = await response.json();
     print(data.modelos);
-    addElement(data.modelos, `model-${veicle}`);
+    addElement(data.modelos, `model-${veicle}`, codigoVeicle);
   } catch (error) {
     print(error);
   }
   // print(url);
 }
+
+const yearVeicle = async (codeVeicle, veicle, marcaVeicle) => {
+  const url = `https://parallelum.com.br/fipe/api/v1/${veicle}/marcas/${marcaVeicle}/modelos/${codeVeicle}/anos`
+  const response = await fetch(url);
+  const data = await response.json();
+  addElement(data, `year-${veicle}`, codeVeicle);
+}
+
+const tablePrice = async () => {
+  const url = `https://parallelum.com.br/fipe/api/v1/${VEICLE.veicleType}/marcas/${VEICLE.codeVeicle}/modelos/${VEICLE.modelVeicle}/anos/${VEICLE.yearVeicle}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  resultTable(data);
+}
+
+// module.exports = tablePrice;
--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+const VEICLE = {};
 // utilitários
 const S = (element) => document.querySelector(element);
 const elem = (element) => document.createElement(element);
 
-function addListener() {
-  const arrTest = document.querySelectorAll('.chosen-model');
+// addListener()
+function addListener(seletor) {
+  const arrTest = document.querySelectorAll(seletor);
   arrTest.forEach((value) => {
     value.addEventListener('click', chosenMarveicle);
   });
 }
-// addListener()
+
+function infoVeicle(veicleType, codeVeicle, modelVeicle, yearVeicle) {
+  VEICLE.veicleType = veicleType;
+  VEICLE.codeVeicle = codeVeicle;
+  VEICLE.modelVeicle = modelVeicle;
+  VEICLE.yearVeicle = yearVeicle;
+  print(VEICLE);
+}
 
 function chosenMarveicle(event) {
   const classVeicleList = event.target.parentElement.className;
@@ -19,14 +28,32 @@ function chosenMarveicle(event) {
     .previousElementSibling.firstElementChild;
 
   // print(inputSelected);
-  inputSelected.value = event.target.innerText;
-  modelVeicle(event.target.id, veicle);
+  if (inputSelected.id === `select-marca-${veicle}`) {
+    inputSelected.value = event.target.innerText;
+    modelVeicle(event.target.id, veicle);
+  }
+
+  if (inputSelected.id === `select-model-${veicle}`) {
+    inputSelected.value = event.target.innerText;
+    const marcaVeicle = event.target.parentElement.id;
+    yearVeicle(event.target.id, veicle, marcaVeicle);
+  }
+
+  if (inputSelected.id === `select-year-${veicle}`) {
+    inputSelected.value = event.target.innerText;
+    const codeVeicle = S(`.results-for-chosen-model-${veicle}`).id;
+    const modelVeicle = S(`.results-for-chosen-year-${veicle}`).id;
+    const yearVeicle = event.target.id;
+    infoVeicle(veicle, codeVeicle, modelVeicle, yearVeicle);
+  }
 }
 
-// Criar lista de marcas de veículos
-function addElement(arr, veicleInfo) {
-  const ul = S(`.results-for-chosen-${veicleInfo}`);
-  ul.innerHtml = '';
+// Criar lista de veículos
+function addElement(arr, veicleClass, codeVeicle) {
+  const ul = S(`.results-for-chosen-${veicleClass}`);
+  ul.innerHTML = '';
+  if (codeVeicle) ul.id = codeVeicle;
+
   arr.forEach((veicle) => {
     const li = elem('li');
     li.innerText = veicle.nome;
@@ -34,5 +61,44 @@ function addElement(arr, veicleInfo) {
     li.id = veicle.codigo;
     ul.appendChild(li);
   })
-  addListener();
+  addListener('.chosen-model');
 }
+
+function resultTable(obj) {
+  const main = S('main');
+  let section = S('.result');
+
+  if (section) {
+    section.innerHTML = '';
+  } {
+    section = elem('section')
+    section.className = 'result';
+  }
+  const table = elem('table');
+  const tbody = elem('tbody');
+
+  const arr = Object.entries(obj)
+  arr.pop();
+  arr.pop();
+  arr.forEach((value) => {
+    const td1 = elem('td');
+    const td2 = elem('td');
+    td1.innerText = value[0];
+    td2.innerText = value[1];
+
+    const tr = elem('tr');
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+    tbody.appendChild(tr);
+    table.appendChild(tbody);
+  });
+
+  section.appendChild(table);
+  main.appendChild(section);
+}
+
+const btns = document.querySelectorAll('button');
+btns.forEach((btn) => {
+  btn.addEventListener('click', tablePrice);
+});
